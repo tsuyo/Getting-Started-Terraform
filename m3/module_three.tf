f251b586b5abeb608008de6e907fa0084cf3cc3b
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2020-04-21 00:53:02 +0900

    finish m3

--- m3/module_three.tf
+++ m3/module_three.tf
@@ -7,7 +7,7 @@ variable "aws_secret_key" {}
 variable "private_key_path" {}
 variable "key_name" {}
 variable "region" {
-  default = "us-east-1"
+  default = "ap-northeast-1"
 }
 
 ##################################################################################
